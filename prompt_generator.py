b9562fe9c71fb4ba396af4220002c270e3a6c5c6
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-02 18:25:35 -0300

    Add typo generating to prompt generator

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,3 +1,23 @@
+import typo
+
 class PromptGenerator:
-    def __init__(self):
-        pass
+    def __init__(self, prompt, problem, technique = 0, variation = 0):
+        self.prompt = prompt
+        self.problem = problem
+        self.technique = technique
+        self.variation = variation
+    
+
+    def generate_prompt_series(self):
+        # problem here refers to the codename I have given the problem or sth
+        return f"{self.problem}_t{self.technique}_v{self.variation}"
+    
+    def generate_modified_prompt(self):
+        if self.variation == 1:
+            self.prompt = self._generate_typo(self.prompt)
+
+        return self.prompt
+
+    def _generate_typo(prompt):
+        return typo.StrErrer(prompt)
+         
