d54cf7285d0f2e1701347d8385c4eb5b952c25fb
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-09 19:13:12 -0300

    Add check if problem_id exists

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from utils import extract_prompt_from_problem
 
 model = "gpt-3.5-turbo"
 language = "Python"
-problem_id = "HE4"
+problem_id = "HE162"
 prompt_type = "0-shot"
 
 if __name__ == "__main__":
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
-    prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id)
-    prompt_series = prompt_gen.generate_prompt_series()
-    code_gen = CodeGenerator(model=model, language=language)
-    response = code_gen.generate_code(prompt_series, prompt)
+
+    if prompt:
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id)
+        prompt_series = prompt_gen.generate_prompt_series()
+        code_gen = CodeGenerator(model=model, language=language)
+        response = code_gen.generate_code(prompt_series, prompt)
