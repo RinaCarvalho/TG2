3ee0df8ab1f5cd2e2cf0a30cefa98de7afd04015
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-11 01:48:12 -0300

    Add evaluator call

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,8 @@
 from evaluator import Evaluator
 
+log_file = "HE4_t0_v0__2023_09_08_03_05_26.json"
+
 if __name__ == "__main__":
-    evaluator = Evaluator()
+    evaluator = Evaluator(log_file=log_file)
+    evaluator.run_test()
     pass
