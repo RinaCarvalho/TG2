b48a3ca682ce461edca4b4c5cfeda566ad56463e
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-14 02:09:58 -0300

    Add run test calls

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,17 @@
 from evaluator import Evaluator
+from utils import extract_tests_from_problem, write_test_results_to_log
 
 log_file = "HE4_t0_v0__2023_09_08_03_05_26.json"
 
 if __name__ == "__main__":
     evaluator = Evaluator(log_file=log_file)
-    evaluator.run_test()
-    pass
+    tests = extract_tests_from_problem(evaluator.problem_id)
+    try:
+        evaluator.run_test(tests)
+        results = "Tests passed successfully"
+
+    except AssertionError as e:
+        results = f"Test failed: {e}"
+
+    finally:
+        write_test_results_to_log(evaluator.log_filepath, results)
