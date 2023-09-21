726ef790d9a680f79bac76162cb3d28135afdab6
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-21 02:08:42 -0300

    Run all tests

--- run_tests.py
+++ run_tests.py
@@ -1,17 +1,21 @@
+import os
+
 from evaluator import Evaluator
 from utils import extract_tests_from_problem, write_test_results_to_log, write_to_jsonl
 
-log_file = "HE4_t0_v0__2023_09_08_03_05_26.json"
+# log_file = "HE31_t0_v0__2023_09_09_18_52_16.json"
 
 if __name__ == "__main__":
     write_to_jsonl()
-    evaluator = Evaluator(log_file=log_file)
-    tests = extract_tests_from_problem(evaluator.problem_id)
-    try:
-        evaluator.run_test(tests)
-        results = "Tests passed successfully"
 
-    except AssertionError as e:
-        results = f"Test failed: {e}"
+    for log_file in os.listdir("logs"):
+        evaluator = Evaluator(log_file=log_file)
+        tests = extract_tests_from_problem(evaluator.problem_id)
+        try:
+            evaluator.run_test(tests)
+            results = "Tests passed successfully"
 
-    write_test_results_to_log(evaluator.log_filepath, results)
+        except AssertionError as e:
+            results = f"Test failed: {e}"
+
+        write_test_results_to_log(evaluator.log_filepath, results)
