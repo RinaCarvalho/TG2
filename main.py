5f0ea6e37a5ed6a8672d0b248e7faa6891afc0d4
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-24 02:33:23 -0300

    Start typo prompting

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # TODO: Everywhere: document methods (docstrings)
 
 from prompt_generator import PromptGenerator
+from prompt_constants import Variation
 from code_generator import CodeGenerator
 from utils import extract_prompt_from_problem, write_to_jsonl
 
 model = "gpt-3.5-turbo"
 language = "Python"
-problem_id = "HE162"
+problem_id = "HE4"
 prompt_type = "0-shot"
 
 if __name__ == "__main__":
@@ -14,7 +15,8 @@ if __name__ == "__main__":
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
 
     if prompt:
-        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id)
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, variation=Variation.TYPO)
         prompt_series = prompt_gen.generate_prompt_series()
+        prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
-        code_gen.generate_code(prompt_series, prompt, 7)
+        code_gen.generate_code(prompt_series, prompt, 1)
