1e5b6a342824ca1987c6eb598a379ff6a6f87d85
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-24 02:32:53 -0300

    Specify log directory

--- run_tests.py
+++ run_tests.py
@@ -4,12 +4,13 @@ from evaluator import Evaluator
 from utils import extract_tests_from_problem, write_test_results_to_log, write_to_jsonl
 
 # log_file = "HE31_t0_v0__2023_09_09_18_52_16.json"
+log_directory = "logs/GPT_35_t0_v1"
 
 if __name__ == "__main__":
     write_to_jsonl()
 
-    for log_file in os.listdir("logs"):
-        evaluator = Evaluator(log_file=log_file)
+    for log_file in os.listdir(log_directory):
+        evaluator = Evaluator(log_file=log_file, log_directory=log_directory)
         tests = extract_tests_from_problem(evaluator.problem_id)
 
         if not tests:
