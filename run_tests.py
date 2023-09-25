167e8c63a45de227eeefca5a41bcdd495964996b
RinaCarvalho <rina.carvalho381@gmail.com>
2023-09-25 13:59:53 -0300

    Run tests for few shot with typo prompting

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,9 @@
 import os
-
 from evaluator import Evaluator
 from utils import extract_tests_from_problem, write_test_results_to_log, write_to_jsonl
 
 # log_file = "HE31_t0_v0__2023_09_09_18_52_16.json"
-log_directory = "logs/GPT_35_t0_v1"
+log_directory = "logs/GPT_35_t1_v1"
 
 if __name__ == "__main__":
     write_to_jsonl()
