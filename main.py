146b3a2adda8ab1546c0cad1bab4a171678fbf5f
RinaCarvalho <rina.carvalho381@gmail.com>
2023-10-08 04:24:06 -0300

    Generate code with formatting errors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils import extract_prompt_from_problem, write_to_jsonl
 
 model = "gpt-3.5-turbo"
 language = "Python"
-problem_id = "HE6"
+problem_id = "HE162"
 prompt_type = "0-shot"
 log_path = "logs/GPT_35_t1_v3"
 
@@ -20,4 +20,4 @@ if __name__ == "__main__":
         prompt_series = prompt_gen.generate_prompt_series()
         prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
-        code_gen.generate_code(prompt_series, prompt, 10, log_path)
+        code_gen.generate_code(prompt_series, prompt, 2, log_path)
