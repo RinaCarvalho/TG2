16af7b49e5bf88d061838694833f7a1466f59e13
RinaCarvalho <rina.carvalho381@gmail.com>
2023-10-15 03:40:41 -0300

    Generate code for MBPP few-shot with typos

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from utils import extract_prompt_from_problem, write_to_jsonl
 
 model = "gpt-3.5-turbo"
 language = "Python"
-problem_id = "MBPP783"
+problem_id = "MBPP12"
 prompt_type = "0-shot"
-log_path = "logs/GPT_35_t1_v0"
+log_path = "logs/GPT_35_t1_v2"
 
 if __name__ == "__main__":
     write_to_jsonl()
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
 
     if prompt:
-        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.FEW_SHOT, variation=Variation.NONE)
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.FEW_SHOT, variation=Variation.INACCURACY)
         prompt_series = prompt_gen.generate_prompt_series()
         prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
