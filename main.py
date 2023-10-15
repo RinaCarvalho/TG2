b3c835775d3d002f101f68371b1d1a9f1ebcff7b
RinaCarvalho <rina.carvalho381@gmail.com>
2023-10-15 03:18:49 -0300

    Generate code for MBPP few-shot

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from utils import extract_prompt_from_problem, write_to_jsonl
 
 model = "gpt-3.5-turbo"
 language = "Python"
-problem_id = "MBPP765"
+problem_id = "MBPP783"
 prompt_type = "0-shot"
-log_path = "logs/GPT_35_t0_v1"
+log_path = "logs/GPT_35_t1_v0"
 
 if __name__ == "__main__":
     write_to_jsonl()
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
 
     if prompt:
-        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.ZERO_SHOT, variation=Variation.TYPO)
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.FEW_SHOT, variation=Variation.NONE)
         prompt_series = prompt_gen.generate_prompt_series()
         prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
