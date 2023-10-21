c87a34531317a6fa839699c276860466459c6d9c
RinaCarvalho <rina.carvalho381@gmail.com>
2023-10-21 20:22:00 -0300

    Generate code for CC1575A few shot

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ model = "gpt-3.5-turbo"
 language = "Python"
 problem_id = "CC1575A"
 prompt_type = "0-shot"
-log_path = "logs/GPT_35_t0_v1"
+log_path = "logs/GPT_35_t1_v0"
 
 if __name__ == "__main__":
     write_to_jsonl()
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
 
     if prompt:
-        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.ZERO_SHOT, variation=Variation.TYPO)
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.FEW_SHOT, variation=Variation.NONE)
         prompt_series = prompt_gen.generate_prompt_series()
         prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
-        code_gen.generate_code(prompt_series, prompt, 9, log_path)
+        code_gen.generate_code(prompt_series, prompt, 10, log_path)
