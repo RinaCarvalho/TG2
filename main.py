b7956fe1eb6050a5b099187675ae5dc0484b6472
RinaCarvalho <rina.carvalho381@gmail.com>
2023-11-02 16:37:16 -0300

    Refactor main.py

--- main.py
+++ main.py
@@ -3,21 +3,35 @@
 from prompt_generator import PromptGenerator
 from prompt_constants import Technique, Variation
 from code_generator import CodeGenerator
-from utils import extract_prompt_from_problem, write_to_jsonl
+from utils import extract_prompt_from_problem, write_to_jsonl, list_problem_ids
 
 model = "gpt-4"
 language = "Python 3"
-problem_id = "YTDL6"
+problem_id = None
 prompt_type = "0-shot"
-log_path = "logs/GPT_4_t0_v0"
+log_path = "logs/GPT_4_t1_v0"
+technique=Technique.FEW_SHOT
+variation=Variation.NONE
+samples = 1
 
-if __name__ == "__main__":
-    write_to_jsonl()
+
+def generate_code_for_problem(problem_id, prompt_type, technique, variation, model, language, log_path, samples):
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
-
+    
     if prompt:
-        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.ZERO_SHOT, variation=Variation.NONE)
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=technique, variation=variation)
         prompt_series = prompt_gen.generate_prompt_series()
         prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
-        code_gen.generate_code(prompt_series, prompt, 1, log_path)
+        code_gen.generate_code(prompt_series, prompt, samples, log_path)
+
+
+if __name__ == "__main__":
+    write_to_jsonl()
+
+    if problem_id:
+        generate_code_for_problem(problem_id, prompt_type, technique, variation, model, language, log_path, samples)
+    else:
+        problem_ids = list_problem_ids()
+        for problem_id in problem_ids:
+            generate_code_for_problem(problem_id, prompt_type, technique, variation, model, language, log_path, samples)
