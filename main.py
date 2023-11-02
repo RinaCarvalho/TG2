64e8cafc42ea0afd2802a051c2b8f789f874d413
RinaCarvalho <rina.carvalho381@gmail.com>
2023-11-02 15:17:11 -0300

    Add GPT4 0-shot

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from prompt_constants import Technique, Variation
 from code_generator import CodeGenerator
 from utils import extract_prompt_from_problem, write_to_jsonl
 
-model = "gpt-3.5-turbo"
+model = "gpt-4"
 language = "Python 3"
-problem_id = "CC1580B"
+problem_id = "YTDL6"
 prompt_type = "0-shot"
-log_path = "logs/GPT_35_t1_v3"
+log_path = "logs/GPT_4_t0_v0"
 
 if __name__ == "__main__":
     write_to_jsonl()
     prompt = extract_prompt_from_problem(problem_id=problem_id, prompt_type=prompt_type)
 
     if prompt:
-        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.FEW_SHOT, variation=Variation.FORMATTING)
+        prompt_gen = PromptGenerator(prompt=prompt, problem=problem_id, technique=Technique.ZERO_SHOT, variation=Variation.NONE)
         prompt_series = prompt_gen.generate_prompt_series()
         prompt = prompt_gen.generate_modified_prompt()
         code_gen = CodeGenerator(model=model, language=language)
-        code_gen.generate_code(prompt_series, prompt, 10, log_path)
+        code_gen.generate_code(prompt_series, prompt, 1, log_path)
--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ from evaluator import Evaluator
 from utils import extract_tests_from_problem, write_test_results_to_log, write_to_jsonl
 
 # log_file = "HE31_t0_v0__2023_09_09_18_52_16.json"
-log_directory = "logs/GPT_35_t1_v3"
+log_directory = "logs/GPT_4_t0_v0"
 
 if __name__ == "__main__":
     write_to_jsonl()
